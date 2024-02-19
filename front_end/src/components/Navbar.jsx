aa4ac7765fcc800b326b51880cf9c32751185832
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-19 10:01:00 +0200

    Add Material-UI components to Navbar

--- front_end/src/components/Navbar.jsx
+++ front_end/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@mui/material";
 
 const Container = styled.div`
   height: 60px;
@@ -9,8 +11,11 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 const Left = styled.div`
+  display: flex;
+  align-items: center;
   flex: 1;
 `;
 
@@ -19,12 +24,39 @@ const Language = styled.span`
   font-size: 14px;
 `;
 
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid lightgray;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+  background-color: transparent;
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -33,10 +65,23 @@ const Navbar = () => {
       <Wrapper>
         <Left>
           <Language>EN</Language>
-          <SearchContainer></SearchContainer>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo>Glittering Rock Jewells</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
