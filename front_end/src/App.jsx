03065529ba35664fff3633d3f51034a3fefa87f2
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-20 00:09:27 +0200

    Add Register and Login pages to App.jsx

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,12 +1,16 @@
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export const App = () => {
   return (
     // <Home />
     // <ProductList />
-    <Product />
+    // <Product />
+    // <Register />
+    <Login />
   );
 };
 
---
+++ front_end/src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div``;
+
+const Title = styled.h1``;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
