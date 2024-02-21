a40061bacebca6bd6c66350fcc34e1168709e2cd
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-21 21:42:21 +0200

    Add dependencies and update user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,29 @@
 import { Router } from "express";
+import { verifyTokenAndAuthorization } from "./verifyToken.js";
 
 const router = Router();
 
+// Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 export default router;
