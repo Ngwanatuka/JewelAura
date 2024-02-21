adb4af753d6360719419fb3ec717bc00ff52f58f
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-21 14:27:09 +0200

    Added User model schema

--- api/index.js
+++ api/index.js
@@ -1,22 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    process.env.MONGO_URL,
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error: ", err));
 
 const app = express();
 
-app.get("/api/test", ()=> {
-    console.log("Test API");
-})
+app.use(express.json());
+
+app.get("/api/test", () => {
+  console.log("Test API");
+});
+
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running");
