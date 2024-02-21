ef8b891de52f046f976cf3929ee4bc45868d5199
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-21 13:42:48 +0200

    Add MONGO_URL and PORT environment variables

--- api/index.js
+++ api/index.js
@@ -14,6 +14,10 @@ mongoose
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.get("/api/test", ()=> {
+    console.log("Test API");
+})
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server is running");
 });
