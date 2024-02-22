8026b267d2a20994730a08818fc9172fb83f1f2e
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-22 10:38:40 +0200

    Refactor user route and token verification

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,29 +1,31 @@
 import { Router } from "express";
 import { verifyTokenAndAuthorization } from "./verifyToken.js";
+import User from "../models/User.js";
 
 const router = Router();
 
-// Update
+//UPDATE
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-  if (req.body.password) {
-    req.body.password = CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASS_SEC
-    ).toString();
-  }
-
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+      ).toString();
+    }
+  
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+        console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
 export default router;
