ab91a7b28fd18ef1b985bc60d7904181ec7ed2aa
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-06 04:56:05 +0200

    Update STRIPE_SECRET_KEY to STRIPE_KEY

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,7 +1,7 @@
 import express from "express";
 import Stripe from "stripe"
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_KEY);
 
 const router = express.Router();
 
