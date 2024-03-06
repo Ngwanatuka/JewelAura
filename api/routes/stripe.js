0222476e8e6a316e55d7c08805143592d60d0c5c
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-06 04:54:57 +0200

    Refactor Stripe API route to use ES6 syntax and update Stripe module

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,21 +1,25 @@
-import router from 'express';
-import stripe from 'stripe';
+import express from "express";
+import Stripe from "stripe"
 
-const stripe = stripeModule(process.env.STRIPE_SECRET_KEY);
-
-router.post("/payment", (req, res) => {
-    stripe.charges.create({
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: "usd",
-    } , (stripeErr, stripeRes) => {
-        if(stripeErr) {
-            res.status(500).send({ error: stripeErr });
-        } else {
-            res.status(200).send({ success: stripeRes });
-        }
-    });
-});
-
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const router = express.Router();
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.token.id,
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).send({ error: stripeErr });
+      } else {
+        res.status(200).send({ success: stripeRes });
+      }
+    }
+  );
+});
+
+export default router;
