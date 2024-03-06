d685fc10a18bfe05df70d3025a00e1492eeb7c6c
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-06 02:32:38 +0200

    Refactor code to improve performance and readability

---
+++ stripe/pages/Success.jsx
@@ -0,0 +1,10 @@
+const Success = () => {
+    return (
+        <div>
+            <h2>Payment Success</h2>
+            <p>Thank you for your payment.</p>
+        </div>
+    )
+};
+
+export default Success;
