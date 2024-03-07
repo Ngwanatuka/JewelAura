2817ba869817acabe6a9746584cd5a9c8f3eda4b
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-07 05:32:43 +0200

    Fix stripeToken in Cart.jsx

--- front_end/src/pages/Cart.jsx
+++ front_end/src/pages/Cart.jsx
@@ -177,7 +177,7 @@ const Cart = () => {
     const makeRequest = async () => {
       try {
         const res = await userRequest.post("/checkout/payment", {
-          tokenId: stripeToken,
+          tokenId: stripeToken.id,
           amount: cart.total * 100,
         });
         navigate("/success");
