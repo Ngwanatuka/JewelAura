75b9f0e18796a5e9197a5b1de932223548b6d293
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-07 03:03:23 +0200

    Refactor PayPalButtons and Products components

--- front_end/src/components/Products.jsx
+++ front_end/src/components/Products.jsx
@@ -13,6 +13,9 @@ const Container = styled.div`
 `;
 
 const Products = ({cat, filters, sort}) => {
+
+  console.log(cat, filters, sort);
+
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
