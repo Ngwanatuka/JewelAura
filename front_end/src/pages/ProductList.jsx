498c1bd3dd8888ef46c8fa8fe5506ab89b099532
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-07 02:22:11 +0200

    Remove unnecessary files and configurations

--- front_end/src/pages/ProductList.jsx
+++ front_end/src/pages/ProductList.jsx
@@ -4,13 +4,10 @@ import Footer from "../components/Footer";
 import Announcements from "../components/Announcements";
 import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
-import { mobile } from "../responsive";
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
 
 const Container = styled.div``;
 
-const Title = styled.h1`
+const Tittle = styled.h1`
   margin: 20px;
 `;
 
@@ -21,71 +18,55 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
-  ${mobile({ width: "0px 20px", flexDirection: "column", display: "flex" })}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
+
   margin-right: 20px;
-  ${mobile({ marginRight: "0px" })}
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
   cursor: pointer;
-  ${mobile({ margin: "10px 0px" })}
 `;
 
-const Option = styled.option``;
-
 const ProductList = () => {
-  const location = useLocation();
-  const cat = location.pathname.split("/")[2];
-  const [filters, setFilters] = useState({});
-  const [sort, setSort] = useState("newest");
-
-  const handleFilters = (e) => {
-    const value = e.target.value;
-    setFilters({
-      ...filters,
-      [e.target.name]: value,
-    });
-  };
   return (
     <Container>
       <Announcements />
       <Navbar />
-      <Title>{cat}</Title>
+      <Tittle>Rings</Tittle>
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products:</FilterText>
-          <Select name="color" onChange={handleFilters}>
-            <Option disabled>Color</Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>
+          <Select>
+            <option disabled>Color</option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
+            <option value="red">Red</option>
+            <option value="blue">Blue</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
           </Select>
-          <Select name="size" onChange={handleFilters}>
-            <Option disabled>Size</Option>
-            <Option>Small</Option>
-            <Option>Medium</Option>
-            <Option>Large</Option>
+          <Select>
+            <option disabled>Size</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
           </Select>
         </Filter>
         <Filter>
           <FilterText>Sort Products:</FilterText>
-          <Select onChange={(e) => setSort(e.target.value)}>
-            <Option value="newest">Newest</Option>
-            <Option value="asc">Price (asc)</Option>
-            <Option value="desc">Price (desc)</Option>
+          <Select>
+            <option value="newest">Newest</option>
+            <option value="asc">Price (asc)</option>
+            <option value="desc">Price (desc)</option>
           </Select>
         </Filter>
       </FilterContainer>
-      <Products cat={cat} filters={filters} sort={sort} />
+      <Products />
       <Newsletter />
       <Footer />
     </Container>
