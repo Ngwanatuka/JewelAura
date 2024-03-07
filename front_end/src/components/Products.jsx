81d728214ac877cc8dfe8af13cdeba639dee51b9
Ngwanatuka <ngwanatuka@gmail.com>
2024-03-07 03:25:39 +0200

    Refactor Products component to fetch products based on category and apply filters

--- front_end/src/components/Products.jsx
+++ front_end/src/components/Products.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { popularProducts } from "../data";
 import Product from "./Product";
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
@@ -12,26 +11,43 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
-const Products = ({cat, filters, sort}) => {
-
+const Products = ({ cat, filters, sort }) => {
   console.log(cat, filters, sort);
 
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(() => {
-    const getProducts =async () => {
+    const getProducts = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/products");
-        console.log(res);
-      } catch (error) {};
-    }
-  },[cat]); 
+        const res = await axios.get(
+          cat
+            ? `http://localhost:5000/api/products?category=${cat}`
+            : "http://localhost:5000/api/products"
+        );
+        setProducts(res.data);
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getProducts();
+  }, [cat]);
 
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, cat, filters]);
 
   return (
     <Container>
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
     </Container>
