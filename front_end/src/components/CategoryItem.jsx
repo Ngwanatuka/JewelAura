7563928808283ffb33c5504a0a981fc1e7e8c2b7
ngmolepjhb024 <ngmolepjhb024@student.wethinkcode.co.za>
2025-06-05 08:42:23 +0200

    UI: component size consistency

--- front_end/src/components/CategoryItem.jsx
+++ front_end/src/components/CategoryItem.jsx
@@ -6,14 +6,19 @@ import { Link } from "react-router-dom";
 const Container = styled.div`
   flex: 1;
   margin: 3px;
-  height: 70vh;
+  height: 30vh;
   position: relative;
+  &:hover {
+    transform: translateY(-5px);
+    transition: all 0.3s ease;
+  }
 `;
 
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  border-radius: 10px;
   ${mobile({ height: "30vh" })}
 `;
 
@@ -37,12 +42,23 @@ const Title = styled.h1`
 `;
 
 const Button = styled.button`
+  width: 40%;
   border: none;
-  padding: 10px;
-  background-color: white;
-  color: gray;
+  padding: 15px 20px;
+  background-color:rgb(54, 132, 132);
+  color: white;
   cursor: pointer;
-  font-weight: 600;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color:rgb(110, 152, 152);
+    
+  }
+  &:disabled {
+    background-color: #9e9e9e;
+    cursor: not-allowed;
+  }
 `;
 
 const CategoryItem = ({ item }) => {
