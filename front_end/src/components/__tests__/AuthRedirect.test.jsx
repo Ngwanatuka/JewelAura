ef93aa83f4935dd630ef869117cd11ce8ccf7d54
Ngwanatuka <ngmolepjhb024@student.wethinkcode.co.za>
2025-10-28 16:22:37 +0200

    fix: remove unused imports in AuthRedirect and Checkout test files

--- front_end/src/components/__tests__/AuthRedirect.test.jsx
+++ front_end/src/components/__tests__/AuthRedirect.test.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import Product from '../../components/Product';
-import App from '../../App';
 import cartReducer from '../../redux/cartRedux';
 import userReducer from '../../redux/userRedux';
 
